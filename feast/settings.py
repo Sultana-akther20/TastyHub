4a83686a798ab4bbabf9f20877ac9e8602af742c
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-06-15 18:56:20 +0100

    Remove hardcoded secret key for security

--- feast/settings.py
+++ feast/settings.py
@@ -24,10 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d1k97$v9&y$%z9+$#zd*4$12n(&o7o1qr=ci*crw^uv(ob2c8!'
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/ 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -55,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
