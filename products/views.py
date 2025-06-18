ee9f5784bdb63efd78666eeb9512fbebcb37fb03
Sultana-akther20 <Sultana.akther2805@gmail.com>
2025-06-18 10:07:08 +0100

    Add the query for searching

--- products/views.py
+++ products/views.py
@@ -1,22 +1,35 @@
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 #from django.views.generic import ListView, DetailView
-#from django.db.models import Q, Prefetch, Avg
+from django.db.models import Q
 #from django.http import JsonResponse
 #from django.core.paginator import Paginator
-#from django.views import View
+from django.contrib import messages
 from .models import Product, Category, ProductImage, ProductReview
 
 def all_products(request):
     """Display all products including filtering and searching"""
 
     products = Product.objects.all()
-    context = {
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET.get('q')
+        if not query:
+            messages.error(request, "Please enter a search term.")
+            return redirect(reverse('products'))
+        
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
+
+        context = {
         'products': products,
         'categories': Category.objects.filter(is_active=True, parent=None).order_by('order'),
         'current_category': request.GET.get('category', ''),
-        'search_query': request.GET.get('q', ''),
-    }
+        #'search_query': request.GET.get('q', ''),
+        'search_term': query,
+        }
+    
     return render(request, 'products/products.html', context)
 
 
